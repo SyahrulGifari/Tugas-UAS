5c879924a80d5c30a589212865f3aac0f7f51063
syahr <syahr@SYAHRULPC>
2023-02-09 20:44:41 +0700

    Add project files.

---
+++ Tugas UAS/FormPerulangan.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtTuliskanText = New System.Windows.Forms.TextBox()
+        Me.txtJmlPerulangan = New System.Windows.Forms.TextBox()
+        Me.btnProses = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'txtTuliskanText
+        '
+        resources.ApplyResources(Me.txtTuliskanText, "txtTuliskanText")
+        Me.txtTuliskanText.Name = "txtTuliskanText"
+        '
+        'txtJmlPerulangan
+        '
+        resources.ApplyResources(Me.txtJmlPerulangan, "txtJmlPerulangan")
+        Me.txtJmlPerulangan.Name = "txtJmlPerulangan"
+        '
+        'btnProses
+        '
+        resources.ApplyResources(Me.btnProses, "btnProses")
+        Me.btnProses.Name = "btnProses"
+        Me.btnProses.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        resources.ApplyResources(Me.btnClear, "btnClear")
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        resources.ApplyResources(Me.btnQuit, "btnQuit")
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        resources.ApplyResources(Me.ListBox1, "ListBox1")
+        Me.ListBox1.Name = "ListBox1"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnProses)
+        Me.Controls.Add(Me.txtJmlPerulangan)
+        Me.Controls.Add(Me.txtTuliskanText)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtTuliskanText As TextBox
+    Friend WithEvents txtJmlPerulangan As TextBox
+    Friend WithEvents btnProses As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Tugas UAS/FormPerulangan.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtTuliskanText.Text = ""
+        txtJmlPerulangan.Text = ""
+
+
+    End Sub
+
+    Private Sub btnProses_Click(sender As Object, e As EventArgs) Handles btnProses.Click
+
+        If txtTuliskanText.Text = "" Then
+            MessageBox.Show("Input tidak boleh kosong!", "Pesan Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+        Dim teks As String
+        Dim jumlah As Integer
+        teks = txtTuliskanText.Text
+        jumlah = txtJmlPerulangan.Text
+
+
+
+        For i = 1 To jumlah
+            ListBox1.Items.Add(teks)
+        Next i
+
+    End Sub
+
+
+End Class
